8fdbb68e3daddf2b843e01ffc3c2ece4d4cb6c0d
atharvadixit <atharvadixit15@gmail.com>
2021-05-22 12:24:41 +0530

    Now supports cross origins, user data fetch

--- src/main/java/com/example/Spring_Server/Controller.java
+++ src/main/java/com/example/Spring_Server/Controller.java
@@ -1,6 +1,14 @@
 package com.example.Spring_Server;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.core.instrument.util.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,8 +17,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class Controller {
 
-    @RequestMapping("/hello")
-    public String sayHello() {
-        return "Hello World!";
+    @CrossOrigin(origins = "https://client-side-react.herokuapp.com")
+    @RequestMapping("/getUserData")
+    public String userData() throws IOException{
+        String fileName = "static/sample.txt";
+        ClassLoader classLoader = getClass().getClassLoader();
+        String result = "";
+        try (InputStream inputStream = classLoader.getResourceAsStream(fileName)) {
+            result = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
